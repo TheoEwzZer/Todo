84da2a635a080770bf01785db806011a43b325a6
Théo <theo.fabianomattei@gmail.com>
2023-06-03 16:13:19 +0200

    🎨 style: add HomeImage component to the home page to improve visual appeal

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import Header from "../components/Header.tsx";
+import HomeImage from "../components/HomeImage.tsx";
 import Todos from "../components/Todo.tsx";
 
 function Home(): React.ReactElement {
   return (
     <ChakraProvider>
       <Header />
+      <HomeImage />
       <Todos />
     </ChakraProvider>
   );
