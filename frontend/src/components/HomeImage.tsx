e3c2d4370b48afd14e59986eaf79d42ea33ba745
Théo <theo.fabianomattei@gmail.com>
2023-06-03 16:14:34 +0200

    ✨ feat: add HomeImage component to display a image

---
+++ frontend/src/components/HomeImage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Image } from "@chakra-ui/react";
+
+export default function HomeImage(): React.ReactElement {
+  return (
+    <Image
+      src="https://c0.wallpaperflare.com/preview/516/233/193/writing-postcard-letter-pen.jpg"
+      alt="Todo Manager"
+      width="100%"
+      height="300px"
+    />
+  );
+}
